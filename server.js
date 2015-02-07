8d072a52193bc733b8cc07ea436db451d23451c7
Rob Brackett <rob@robbrackett.com>
2015-02-06 17:45:48 -0800

    The simplest possible conversion service.

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
+var topojson = require("topojson");
 var express = require("express");
+var bodyParser = require('body-parser')
 var app = express();
 
 var PORT = process.env.PORT || 3000;
  
 app.get("/", function (request, response) {
   response.send("To use this service, POST GeoJSON data to '/'");
-  console.log("Got request");
 });
 
-app.post("/", function (request, response) {
-  response.send("Sorry, should have returned TopoJSON, getting there.");
-});
+app.post("/",
+  bodyParser.json({ limit: "10mb"}),
+  function (error, request, response, next) {
+    return response.end("You sent malformed JSON data.");
+  },
+  function (request, response) {
+    // TODO: validate posted data
+    // TODO: accept options for transforming
+    var topology = topojson.topology({data: request.body});
+    response.end(JSON.stringify(topology));
+  });
  
 app.listen(PORT);
 console.log("topojson-service server listening on port %d", PORT);
